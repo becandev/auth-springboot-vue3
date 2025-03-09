61e3ab5bd15828e9d1f37d986a400f9f1ef94e10
becandev <codezenith.life@gmail.com>
2025-03-09 22:17:45 +0100

    fix: Configure swagger to properly handle jwt

--- backend/src/main/java/com/nidhal/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/nidhal/backend/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.nidhal.backend.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -16,12 +19,23 @@ import org.springframework.context.annotation.Configuration;
     bearerFormat = "JWT",
     in = SecuritySchemeIn.HEADER
 )
+@OpenAPIDefinition(
+    info = @io.swagger.v3.oas.annotations.info.Info(
+        title = "Spring Boot JWT Authentication API",
+        version = "1.0",
+        description = "A simple Spring Boot JWT Authentication API"
+    ),
+    security = {
+        @SecurityRequirement(name = "bearerAuth")
+    }
+)
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+            .components(new Components())
             .info(new Info()
                 .title("Spring Boot JWT Authentication API")
                 .version("1.0")
